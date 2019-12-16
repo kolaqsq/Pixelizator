ce5427b66cb7e11b7e769e29941c0d2bde7a7567
kolaqsq <kolaqsq1707@gmail.com>
2019-12-17 01:33:13 +0300

    Updated UI

--- Pixelizator.py
+++ Pixelizator.py
@@ -5,15 +5,15 @@ from PyQt5.QtWidgets import *
 from PyQt5.uic import loadUi
 
 
-class MainWindow(QMainWindow):
+class MainWindowLight(QMainWindow):
     def __init__(self, *args, **kwargs):
-        super(MainWindow, self).__init__(*args, **kwargs)
-        loadUi("mainWindow.ui", self)
+        super(MainWindowLight, self).__init__(*args, **kwargs)
+        loadUi("MainWindowLight.ui", self)
 
 
 def main():
     app = QtWidgets.QApplication(sys.argv)
-    window = MainWindow()
+    window = MainWindowLight()
     window.show()
     app.exec_()
 
