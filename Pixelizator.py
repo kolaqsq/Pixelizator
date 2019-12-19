b132286b3101889633864a091a20f460af282d6e
kolaqsq <kolaqsq1707@gmail.com>
2019-12-20 02:36:29 +0300

    Fixed picture aspect ratio

--- Pixelizator.py
+++ Pixelizator.py
@@ -1,8 +1,14 @@
 import sys
 
+from PIL import Image
 from PyQt5 import QtWidgets
+from PyQt5.QtCore import *
+from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import *
 from PyQt5.uic import loadUi
+
+img = Image.open("alg-img/testimg.jpg")
+width, height = img.size
 
 
 class MainWindow(QMainWindow):
@@ -13,15 +19,39 @@ class MainWindow(QMainWindow):
         self.settingsHome.clicked.connect(lambda: MainWindow.change(self, 0))
         self.show()
 
-
     def change(self, i):
         self.pages.setCurrentIndex(i)
+
+    def init_ui(self):
+        self.pixelizationLevelSlider.valueChanged[int].connect(self.changeValue)
+
+    def changeValue(self, value):
+        # slider from 1 to 350
+        slider = value
+        scalew = int((width / 1000) * slider)
+        scaleh = int((height / 1000) * slider)
+
+        # Resize smoothly down to scalew x scaleh pixels
+        imgSmall = img.resize((scalew, scaleh), resample=Image.BILINEAR)
+        # Scale back up using NEAREST to original size
+        result = imgSmall.resize(img.size, Image.NEAREST)
+        # Save on jpg or png
+        result.save('alg-img/result.png')
+        self.changeScale()
+
+    def resizeEvent(self, event):
+        self.changeScale()
+
+    def changeScale(self):
+        pix = QPixmap('alg-img/result.png')
+        pix = pix.scaled(self.imageWindow.size(), Qt.KeepAspectRatio, transformMode=Qt.SmoothTransformation)
+        self.imageWindow.setPixmap(pix)
 
 
 def main():
     app = QtWidgets.QApplication(sys.argv)
     window = MainWindow()
-    # window.show()
+    window.init_ui()
     app.exec_()
 
 
